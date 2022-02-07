9427bd90252a3d631e980dba39829b953fb6eff7
AdityaDS <adityads@ymail.com>
2022-02-07 12:06:38 +0700

    logs histo

--- app/Http/Controllers/LogsController.php
+++ app/Http/Controllers/LogsController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Logs_m;
 
 class LogsController extends Controller
 {
     public function index()
     {
         $this->data['title'] = 'Logs';
+        $this->data['logs'] = Logs_m::orderBy('created_at', 'desc')->get();
         return view('logs', $this->data);
     }
 }
---
+++ resources/views/logs.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.index')
+@section('content')
+@include('layouts.alert')
+
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="card mt-5">
+            <div class="card-header bg-dark">
+                <h4 class="card-title text-white">{{$title}}</h4>
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-12 mb-2">
+                        <div class="table-responsive">
+                            <table class="table ">
+                                <thead>
+                                    <tr>
+                                        <th class="text-center">NO</th>                                        
+                                        <th class="text-center">TANGGAL</th>                                        
+                                        <th class="text-center">Keterangan</th>                                        
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach($logs as $v => $i)
+                                    <tr>
+                                        <td class="text-center">{{($v+1)}}</td>
+                                        <td class="text-center">{{date('d-m-Y H:i:s',strtotime($i->created_at))}}</td>
+                                        <td>{{$i->logs_keterangan}}</td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</div>
+@endsection
+
+@section('custom_js')
+@endsection
